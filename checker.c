7a03bb9c5c064f7ad65cfdf3065708788afa4b89
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-07-31 13:54:13 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 
+int language = 0; // 0 for English, 1 for German
+
+typedef struct {
+const char* temp_msg;
+const char* soc_msg;
+const char* charge_rate_msg;
+} Messages;
+
+Messages lang_sets[] = {
+{"Temperature out of range!","State of Charge out of range!","Charge Rate out of range!"},
+{"Temperatur außerhalb des zulässigen Bereichs!","Ladezustand außerhalb des zulässigen Bereichs!","Laderate außerhalb des zulässigen Bereichs!"}
+};
+
 int value_within_range(float val,float min,float max, const char* msg)
-{
+
   if(val < min || val > max)
   {
     printf("%s\n",msg);
@@ -13,9 +26,9 @@ int value_within_range(float val,float min,float max, const char* msg)
 
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-  return value_within_range(temperature,0,45,"Temperature out of range!")
-      && value_within_range(soc,20,80,"State of Charge out of range!")
-      && value_within_range(chargeRate,0,0.8,"Charge Rate out of range!");
+  return value_within_range(temperature,0,45,lang_sets[language].temp_msg)
+      && value_within_range(soc,20,80,lang_sets[language].soc_msg)
+      && value_within_range(chargeRate,0,0.8,lang_sets[language].charge_rate_msg);
     
 }
 
