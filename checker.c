244bccd2d492a901a67e94655190c2149dc41501
Hemanth-A-H <hemanthhegde94@gmail.com>
2024-07-31 15:17:27 +0530

    review comments updated

--- checker.c
+++ checker.c
@@ -14,11 +14,26 @@ Messages lang_sets[] = {
 {"Temperatur außerhalb des zulässigen Bereichs!","Ladezustand außerhalb des zulässigen Bereichs!","Laderate außerhalb des zulässigen Bereichs!"}
 };
 
+void print_fun(const char* msg)
+{
+  printf("%s\n",msg);
+}
+
 int value_within_range(float val,float min,float max, const char* msg)
 {
   if(val < min || val > max)
   {
-    printf("%s\n",msg);
+    print_fun(const char* msg);
+    return 0;
+  }
+    return 1;
+}
+
+int value_within_range_with_no_min_val(float val,float max, const char* msg)
+{
+  if(val > max)
+  {
+    print_fun(const char* msg);
     return 0;
   }
     return 1;
@@ -28,11 +43,15 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
 {
   return value_within_range(temperature,0,45,lang_sets[language].temp_msg)
       && value_within_range(soc,20,80,lang_sets[language].soc_msg)
-      && value_within_range(chargeRate,0,0.8,lang_sets[language].charge_rate_msg);
+      && value_within_range_with_no_min_val(chargeRate,0.8,lang_sets[language].charge_rate_msg);
     
 }
 
 int main() {
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
+
+  language = 1; // for german
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
